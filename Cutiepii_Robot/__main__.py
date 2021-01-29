a5f11abed7d02228d71b9cf96f8ee1ca332ea152
Animaze-Watch-Anime <78212448+Animaze-Watch-Anime@users.noreply.github.com>
2021-01-29 20:16:38 +0530

    Update __main__.py

--- Cutiepii_Robot/__main__.py
+++ Cutiepii_Robot/__main__.py
@@ -51,17 +51,17 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-Welcome user: {}, , I am cutest bot: Cutiepii! 
+Welcome user: {}, , I am cutest bot:  Kanna! 
 
 I am an Anime themed advance group management bot with a lot of Special Features.
 
-You can also make my sister. [Cutiepii Repo](https://github.com/Rajkumar-27/CutiepiiRobot)
+You can also come in [my home](https://t.me/Dragon_Ball_Chat)!
 
 Use /help to know my abilities [^_^](https://telegra.ph/file/d31755c8e0245be16c4f5.png).
 """
 
 HELP_STRINGS = """
-[Cutie](https://telegra.ph/file/d0441d0d700f54b55c9a1.mp4) at your service.
+[Kanna](https://t.me/c/1179315872/5045813) at your service.
 I can help you with following functions in managing group.
 
 *Main* commands available:
@@ -79,7 +79,7 @@ And the following:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-SAITAMA_IMG = "https://telegra.ph/file/d31755c8e0245be16c4f5.png"
+SAITAMA_IMG = "https://t.me/c/1179315872/5045813"
 CUTIEPINGIMG = "https://telegra.ph/file/213a05719a33e0e504cf9.gif"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
